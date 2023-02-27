5882424137cebf212a9810c1adc51056b481331c
Timmy Tompkins <timhageman@Clementine.lan>
2023-02-27 13:21:01 -0600

    working on handle click for 8ball/ 8ball functino

--- src/EightBall.js
+++ src/EightBall.js
@@ -4,6 +4,10 @@ function randomAnswr(arr) {
     const randomIdx = Math.floor(Math.random() * arr.length);
     return arr[randomIdx];
   }
-  
+
+function EightBall(props){
+    const [msg , setMsg] = useState("Think of any question you may have");
+    const [color, setColor] = useState("black");
+}
 
 
