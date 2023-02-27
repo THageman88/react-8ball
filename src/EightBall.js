9468bab8d355c6f3d5b3f961f85402f86ba5de2d
Timmy Tompkins <timhageman@Clementine.lan>
2023-02-27 13:12:25 -0600

    got rid of answers.js so I could add it into eightball easier

--- src/EightBall.js
+++ src/EightBall.js
@@ -1,2 +1,9 @@
-import React from "react";
-import answers from "./answers";
+import React, {useState} from "react";
+
+function randomAnswr(arr) {
+    const randomIdx = Math.floor(Math.random() * arr.length);
+    return arr[randomIdx];
+  }
+  
+
+
